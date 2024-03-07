3ad5212e462a02bff65b9a2ca0e4e1f62c57d56f
AlexanderPaul1983 <alexanderpaul@gmx.de>
2024-03-07 14:38:59 +0100

    get, post commands were added

--- src/QuestionsApp.Web/Handlers/Queries/GetQuestionsQuery.cs
+++ src/QuestionsApp.Web/Handlers/Queries/GetQuestionsQuery.cs
@@ -1,6 +1,16 @@
 namespace QuestionsApp.Web.Handlers.Queries;
 using MediatR;
 
+public class GetQuestionsResponse
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = "";
+    public int Votes { get; set; }
+}
+
+public class GetQuestionsRequest : IRequest<List<GetQuestionsResponse>>
+{ }
+
 public class GetQuestionsQuery : IRequestHandler<GetQuestionsRequest, List<GetQuestionsResponse>>
 {
     public Task<List<GetQuestionsResponse>> Handle(GetQuestionsRequest request, CancellationToken cancellationToken)
